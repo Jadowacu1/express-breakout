6504bcd78241a23d5a7e8cb332b394479709db50
Jessekay <kayjes00@gmail.com>
2024-10-10 11:08:36 +0200

    Add appropriate comments for the line of the codes

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,23 @@
 import  express  from "express";
 import dotenv from "dotenv";
 
-dotenv.config()
+// Load environment variables from a .env file into process.env 
+dotenv.config();
+
+// Create an instance for an Express application
 const app = express();
 
-const port:any =process.env.PORT || 3000;
+// Define the port, it will use the value from the environment variable PORT or default to 3000 if not set
+const port: any = process.env.PORT || 3000;
 
-// get , put, delete, post
-
-app.get("/",(req,res)=>{
+// Define a route for HTTP GET requests to the root URL ("/")
+// When successfully accessed it will send back the response "This is my first express server"
+app.get("/", (req, res) => {
     res.send("This is my first Express server");
 })
 
-// setting listening port
-app.listen(port,()=>{
+// Set up the application to listen on the defined  port
+// Once the server starts successfully, it will log "Server is running on port [port]"
+app.listen(port, () => {
     console.log("Server is running on port:", port);
 })
