6aaf62f4adf0482500b129373c5a4f5dee4c2cef
Jessekay <kayjes00@gmail.com>
2024-11-06 16:02:32 +0530

    Add new feature to handle user model

---
+++ src/database/models/userModel.ts
@@ -0,0 +1,13 @@
+export class User {
+  email!: string;
+  password!: string;
+
+  constructor(email: string, password: string) {
+    this.email;
+    this.password;
+  }
+}
+
+const user = new User('user01@gmail.com', '2121PassWord');
+
+console.log(user);
